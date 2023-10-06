1274afe081c8250a0b04e71530cd550fd3fa1b61
klthemathnerd8 <91514631+klthemathnerd8@users.noreply.github.com>
2023-10-05 21:16:52 -0700

    Delete buildtests directory

--- buildtests/cjs.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "vitest";
-const replitai = require("../dist");
-
-test("cjs import test", async () => {
-  expect(replitai).toBeTruthy();
-  expect(replitai.chat).toBeTruthy();
-});
--- buildtests/esm.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "vitest";
-import * as replitAiStarImport from "../dist";
-import { chat as replitChatImport } from "../dist";
-
-test("esm import test", async () => {
-  expect(replitAiStarImport).toBeTruthy();
-  expect(replitAiStarImport.chat).toBeTruthy();
-  expect(replitChatImport).toBeTruthy();
-});
